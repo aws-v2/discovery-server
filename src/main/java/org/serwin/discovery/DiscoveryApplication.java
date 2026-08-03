80e96e06bdf3653c42910c0fc98f6cf46da4edf4
Qarani-m <emqarani@gmail.com>
2026-08-03 15:23:22 +0300

    feat: add health check endpoint to discovery server

--- src/main/java/org/serwin/discovery/DiscoveryApplication.java
+++ src/main/java/org/serwin/discovery/DiscoveryApplication.java
@@ -1,15 +1,28 @@
 package org.serwin.discovery;
 
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RestController
+@RequestMapping("/api/v1/discovery")
 public class DiscoveryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryApplication.class, args);
-	}
+    @GetMapping("/health")
+    public ResponseEntity<?> health() {
+        return ResponseEntity.ok(Map.of("ping", "pong"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryApplication.class, args);
+    }
 
 }
